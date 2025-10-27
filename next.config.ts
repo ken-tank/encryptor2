6ed1dd343bf3ce548a9ce8fe1752b3c2546b8915
KenTank <ulilalkhaq4@gmail.com>
2025-10-27 10:40:21 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  skipTrailingSlashRedirect: false
 };
 
 export default nextConfig;
